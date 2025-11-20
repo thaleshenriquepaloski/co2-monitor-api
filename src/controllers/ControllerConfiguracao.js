da376afdea2600aa0a10e4f23f950c4d4fd3f95e
Thales Henrique <thaleshenriquepaloski1@gmail.com>
2025-11-19 21:44:35 -0300

    feat: criando novo método para criar nova configuração

--- src/controllers/ControllerConfiguracao.js
+++ src/controllers/ControllerConfiguracao.js
@@ -7,6 +7,16 @@ class ControllerConfiguracao extends Controller {
     constructor() {
         super(configuracaoService);
     };
+
+    async criarNovaConfig(req, res) {
+        try {
+            const dados = req.body;
+            const configCriada = await configuracaoService.criarNovaConfig(dados);
+            return res.status(200).json(configCriada);
+        } catch (error) {
+            return res.status(500).json({ message: error.message });
+        }
+    }
 };
 
 module.exports = ControllerConfiguracao;
