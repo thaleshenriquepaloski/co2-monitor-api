ed9c069644b71afee6d0a463fa2db8147152872c
Thales Henrique <thaleshenriquepaloski1@gmail.com>
2025-11-19 21:41:03 -0300

    refactor: mudando nome do método pra ficar de acordo com o controllador

--- src/routes/configuracaoRoutes.js
+++ src/routes/configuracaoRoutes.js
@@ -7,7 +7,7 @@ const configuracaoRoutes = Router();
 
 configuracaoRoutes
     .get("/registros-configuracao", (req, res) => configuracaoController.pegarRegistros(req, res))
-    .post("/criar-configuracao", (req, res) => configuracaoController.criarRegistro(req, res))
+    .post("/criar-configuracao", (req, res) => configuracaoController.criarNovaConfig(req, res))
     .delete("/deletar-configuracao/:id", (req, res) => configuracaoController.deletarRegistroPorId(req, res))
 
 module.exports = configuracaoRoutes;
