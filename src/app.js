5ee43fc4661e9e3b62c78afdc87767a7042841ef
Thales Henrique <thaleshenriquepaloski1@gmail.com>
2025-11-24 15:48:08 -0300

    feat: integrando middleware de métricas no app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
+const metricsMiddleware = require("./middlewares/metricsMiddleware.js")
 
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
+
+//midleware de métricas
+app.use(metricsMiddleware);
 
 routes(app);
 
